f615d4aaf7464f198aed23bcf23954a635b2b77c
Kumudu Gunasekara <kumudugunasekara@hotmail.com>
2016-04-10 14:50:15 +1000

    Add add, edit, delete ui

--- ng-table-inline-edit/ngTableInlineEdit.Web/app/main/controllers/home.ctrl.js
+++ ng-table-inline-edit/ngTableInlineEdit.Web/app/main/controllers/home.ctrl.js
@@ -5,12 +5,55 @@
         .module('mainApp')
         .controller('homeCtrl', homeCtrl);
 
-    homeCtrl.$inject = []; 
+    homeCtrl.$inject = ['userDataSvc', '$log', 'NgTableParams'];
 
-    function homeCtrl() {
+    function homeCtrl(userDataSvc, $log, NgTableParams) {
         /* jshint validthis:true */
         var vm = this;
         vm.title = 'homeCtrl';
+        vm.data = [];
+        vm.tableParams = null;
+        vm.tableSettings = null;
+        
+        vm.tableParams = new NgTableParams(
+            { page: 1, count: 8 },
+            {
+                total: 100,
+                counts: [],
+                getData: function ($defer, params) {
+                    var count = params.count();
+                    var page = params.page() - 1;
+                    var currentPageData = [];
 
+                    if (vm.data && vm.data.length > 0) {
+                        vm.tableParams.total(vm.data.length);
+
+                        if (vm.data.length > count) {
+                            var startIndex = page * count;
+                            var endIndex = startIndex + count;
+                            currentPageData = vm.data.slice(startIndex, endIndex);
+                        } else {
+                            currentPageData = vm.data;
+                        }
+                    } else {
+                        vm.tableParams.total(0);
+                    }
+
+                    $defer.resolve(currentPageData);
+                }
+            }
+        );
+
+        (function () {
+            userDataSvc.getUsers().then(
+                function (data) {
+                    vm.data = data;
+                    vm.tableParams.reload();
+                }, function rejected(reason) {
+                    $log.error(reason);
+                }, function notify(update) {
+                    $log.info(update);
+                });
+        })();
     }
 })();
